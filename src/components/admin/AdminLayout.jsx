e971bf8ec0991d1665ee965e040ddab82f0abe03
callmeeve <wididiky38@gmail.com>
2023-12-25 18:12:17 +0700

    ..

--- src/components/admin/AdminLayout.jsx
+++ src/components/admin/AdminLayout.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { HiViewGrid } from 'react-icons/hi';
 
 const AdminLayout = () => {
     return (
-        <div className="flex">
+        <div className="flex h-screen">
             {/* Sidebar */}
-            <div className="w-1/4 bg-gray-800 text-white">
+            <div className="flex flex-col w-64 bg-gray-900 text-white">
+                {/* Sidebar header */}
+                <div className="flex items-center justify-center p-4">
+                    <h1 className="text-white text-xl font-bold uppercase">Admin</h1>
+                </div>
                 {/* Sidebar content */}
-                <div className="py-4">
-                    <Link to="/admin" className="px-4 py-2">Dashboard</Link>
-                    {/* <div className="px-4 py-2">Orders</div>
+                <div className="flex flex-col flex-1 overflow-y-auto">
+                    <div className="flex-1 px-2 py-4">
+                        <Link to="/admin" className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700 rounded-lg"><HiViewGrid className='w-6 h-6 mr-2' />Dashboard</Link>
+                        {/* <div className="px-4 py-2">Orders</div>
                     <div className="px-4 py-2">Products</div>
                     <div className="px-4 py-2">Customers</div> */}
+                    </div>
                 </div>
             </div>
 
             {/* Main content */}
-            <div className="w-3/4 h-screen bg-gray-100">
-                <div className="p-12">
+            <div className="w-full bg-gray-100">
+                <div className="flex flex-col flex-1 overflow-y-auto p-24">
                     <Outlet />
                 </div>
             </div>
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import {HiPlusCircle} from 'react-icons/hi'
+import { HiPlusCircle } from 'react-icons/hi'
 
 const Dashboard = () => {
+
+    const status = {
+        tersedia: "Tersedia",
+        habis: "Habis",
+    }
+
+    const categories = {
+        makanan: "Makanan",
+        minuman: "Minuman",
+    }
+
     // Sample data for products table
     const products = [
-        { id: 1, name: 'Product 1', price: 10 },
-        { id: 2, name: 'Product 2', price: 20 },
-        { id: 3, name: 'Product 3', price: 30 },
+        { id: 1, name: 'Product 1', price: 10, stock: status.tersedia, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.", categories: categories.makanan },
+        { id: 2, name: 'Product 2', price: 20, stock: status.habis, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.", categories: categories.minuman },
+        { id: 3, name: 'Product 3', price: 30, stock: status.tersedia, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.", categories: categories.makanan },
     ];
-
-    // Sample data for product sale chart
-    // const chartData = {
-    //     labels: ['Product 1', 'Product 2', 'Product 3'],
-    //     datasets: [
-    //         {
-    //             label: 'Product Sales',
-    //             data: [5, 10, 15],
-    //             backgroundColor: 'rgba(75, 192, 192, 0.6)',
-    //         },
-    //     ],
-    // };
 
     return (
         <div className="container mx-auto">
@@ -29,8 +28,8 @@ const Dashboard = () => {
                         <h2 className="text-2xl font-semibold">Products</h2>
                         <p className="text-gray-500">A list of all the products including the name, description, price, and stock </p>
                     </div>
-                    <button className="flex flex-row items-center bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-3 px-4 rounded-lg shadow-sm">
-                        <HiPlusCircle className="w-6 h-6 mr-2"/>
+                    <button className="flex items-center bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-3 px-4 rounded-lg shadow-sm">
+                        <HiPlusCircle className="w-6 h-6 mr-2" />
                         Add Product
                     </button>
                 </div>
@@ -41,194 +40,50 @@ const Dashboard = () => {
                                 Name
                             </th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Title
+                                Price
                             </th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Status
                             </th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Role
+                                Category
                             </th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Email
+                                Description
                             </th>
-                            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Actions
-                            </th>
+                            <th scope="col" className="px-6 py-3 "/>
                         </tr>
                     </thead>
                     <tbody className="bg-white divide-y divide-gray-200">
-                        <tr>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 h-10 w-10">
-                                        <img className="h-10 w-10 rounded-full" src="https://i.pravatar.cc/150?img=1" alt />
-                                    </div>
-                                    <div className="ml-4">
-                                        <div className="text-sm font-medium text-gray-900">
-                                            Jane Cooper
-                                        </div>
-                                        <div className="text-sm text-gray-500">
-                                            jane.cooper@example.com
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
-                                <div className="text-sm text-gray-500">Optimization</div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    Active
-                                </span>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                Admin
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                jane.cooper@example.com
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                <a href="#" className="ml-2 text-red-600 hover:text-red-900">Delete</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 h-10 w-10">
-                                        <img className="h-10 w-10 rounded-full" src="https://i.pravatar.cc/150?img=1" alt />
-                                    </div>
-                                    <div className="ml-4">
-                                        <div className="text-sm font-medium text-gray-900">
-                                            Jane Cooper
-                                        </div>
-                                        <div className="text-sm text-gray-500">
-                                            jane.cooper@example.com
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
-                                <div className="text-sm text-gray-500">Optimization</div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    Active
-                                </span>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                Admin
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                jane.cooper@example.com
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                <a href="#" className="ml-2 text-red-600 hover:text-red-900">Delete</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 h-10 w-10">
-                                        <img className="h-10 w-10 rounded-full" src="https://i.pravatar.cc/150?img=1" alt />
-                                    </div>
-                                    <div className="ml-4">
-                                        <div className="text-sm font-medium text-gray-900">
-                                            Jane Cooper
-                                        </div>
-                                        <div className="text-sm text-gray-500">
-                                            jane.cooper@example.com
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
-                                <div className="text-sm text-gray-500">Optimization</div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    Active
-                                </span>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                Admin
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                jane.cooper@example.com
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                <a href="#" className="ml-2 text-red-600 hover:text-red-900">Delete</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 h-10 w-10">
-                                        <img className="h-10 w-10 rounded-full" src="https://i.pravatar.cc/150?img=1" alt />
-                                    </div>
-                                    <div className="ml-4">
-                                        <div className="text-sm font-medium text-gray-900">
-                                            Jane Cooper
-                                        </div>
-                                        <div className="text-sm text-gray-500">
-                                            jane.cooper@example.com
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
-                                <div className="text-sm text-gray-500">Optimization</div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                    Active
-                                </span>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                Admin
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                jane.cooper@example.com
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
-                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                <a href="#" className="ml-2 text-red-600 hover:text-red-900">Delete</a>
-                            </td>
-                        </tr>
-                        {/* More rows... */}
-                    </tbody>
-                </table>
-
-                {/* <table className="table-auto w-full">
-                    <thead>
-                        <tr>
-                            <th className="px-4 py-2">ID</th>
-                            <th className="px-4 py-2">Name</th>
-                            <th className="px-4 py-2">Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
                         {products.map((product) => (
                             <tr key={product.id}>
-                                <td className="border px-4 py-2">{product.id}</td>
-                                <td className="border px-4 py-2">{product.name}</td>
-                                <td className="border px-4 py-2">{product.price}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    <div className="text-sm font-medium text-gray-900">{product.name}</div>
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    <div className="text-sm text-gray-900">{product.price}</div>
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${product.stock === 'Habis' ? 'bg-red-100 text-red-800' : 'bg-green-100 text-green-800'}`}>
+                                        {product.stock}
+                                    </span>
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    {product.categories}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    {product.description}
+                                </td>
+                                <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium">
+                                    <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                    <a href="#" className="ml-2 text-red-600 hover:text-red-900">Delete</a>
+                                </td>
                             </tr>
                         ))}
+
                     </tbody>
-                </table> */}
+                </table>
             </div>
-            {/* <div>
-                <h2 className="text-xl font-bold mb-2">Product Sale Chart</h2>
-                <Bar data={chartData} />
-            </div> */}
         </div>
     );
 };
